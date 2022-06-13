bbcfb5be556f365aaa4e6f657e04d3f6d4f230e7
KakashiHatake27 <kakashi.lightningstyle@gmail.com>
2022-06-13 16:29:24 +0200

    Added other pages and some func

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -29,17 +29,29 @@ namespace PoePartThreeFinal
 
         private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
+            if (!args.IsSettingsSelected)
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
 
+                switch (item.Tag.ToString())
+                {
+                    case "page1": ContentFrame.Navigate(typeof(Home)); break;
+                    case "page2": ContentFrame.Navigate(typeof(Vehicle)); break;
+                    case "page3": ContentFrame.Navigate(typeof(Residential)); break;
+                    case "page4": ContentFrame.Navigate(typeof(Vehicle)); break;
+                }
+            }
         }
 
         private void NavView_Loaded(object sender, RoutedEventArgs e)
         {
-
+            ContentFrame.Navigate(typeof(Home));
         }
 
         private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
         {
-
+            if (ContentFrame.CanGoBack)
+                ContentFrame.GoBack();
         }
     }
 }
